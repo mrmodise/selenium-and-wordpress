b743e5f85d2bf59716c5dc25c7fd258068e5cdbd
Morebodi Modise <modisemorebodi@gmail.com>
2017-06-22 21:13:15 +0200

    added Title attribute and logic to check page edit mode

--- WordPressAutomation/NewPostPage.cs
+++ WordPressAutomation/NewPostPage.cs
@@ -6,6 +6,23 @@ namespace WordPressTests
 {
     public class NewPostPage
     {
+        public static object Title
+        {
+            get
+            {
+                var title = Driver.Instance.FindElement(By.Id("title"));
+                
+                if(title != null)
+                {
+                    return title.GetAttribute("value");
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+
         public static void GoTo()
         {
             var menuPosts = Driver.Instance.FindElement(By.Id("menu-posts"));
@@ -31,6 +48,11 @@ namespace WordPressTests
             var message = Driver.Instance.FindElement(By.Id("message"));
             var newPostLink = message.FindElements(By.TagName("a"))[0];
             newPostLink.Click();
+        }
+
+        public static bool IsInEditMode()
+        {
+            return Driver.Instance.FindElement(By.Id("icon-edit-pages")) != null;
         }
     }
 }
