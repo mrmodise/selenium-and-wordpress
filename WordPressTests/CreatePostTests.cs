f198befdbe6feb66eafc6255dc58f419fe06a86b
Morebodi Modise <modisemorebodi@gmail.com>
2017-06-22 19:43:13 +0200

    refactored code comments

--- WordPressTests/CreatePostTests.cs
+++ WordPressTests/CreatePostTests.cs
@@ -27,16 +27,26 @@ namespace WordPressTests
                 .WithPassword("selenium")
                 .Login();
 
+            // navigate to the create post page
             NewPostPage.GoTo();
 
+            // create post contents - title, body
             NewPostPage
                 .CreatePost("This is the post test title")
                 .WithBody("Hi, this is the body")
                 .Publish();
 
+            // create post
             NewPostPage.GoToNewPost();
 
+            // check if the post has been created and that the title of the post equals the provided one
             Assert.AreEqual(PostPage.Title, "This is the Title", "Title did not match a new post");
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Driver.Close();
         }
     }
 }
