efdf02306f201dd0d576f073796dacd36610f0a0
Morebodi Modise <modisemorebodi@gmail.com>
2017-06-22 21:13:51 +0200

    added more pages test

---
+++ WordPressAutomation/ListPostPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace WordPressAutomation
+{
+    public static class ListPostPage
+    {
+        public static void GoTo(PostType postType)
+        {
+            switch (postType)
+            {
+                case PostType.Page:
+                    Driver.Instance.FindElement(By.Id("menu-pages")).Click();
+                    Driver.Instance.FindElement(By.Id("All Pages")).Click();
+                    break;
+
+            }
+        }
+
+        public static void SelectPost(string title)
+        {
+            var postLink = Driver.Instance.FindElement(By.LinkText("Sample Page"));
+            postLink.Click();
+        }
+
+        public enum PostType
+        {
+            Page
+        }
+    }
+}
---
+++ WordPressTests/PageTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPressAutomation;
+
+namespace WordPressTests
+{
+    [TestClass]
+    public class PageTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void Can_Create_Basic_Post()
+        {
+            // navigate to site under automation
+            LoginPage
+                .GoTo();
+
+            // test the login functionality
+            LoginPage
+                .LoginAs("selenium")
+                .WithPassword("selenium")
+                .Login();
+
+            // navigate to the list post page
+            ListPostPage.GoTo(PostType.Page);
+
+            // select page/post to select
+            ListPostPage.SelectPost("Sample Page");
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Driver.Close();
+        }
+    }
+}
