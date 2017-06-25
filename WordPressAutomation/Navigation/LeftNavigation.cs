79aef680e6b756fac4832116b74e6ab8288e8a2d
Morebodi Modise <modisemorebodi@gmail.com>
2017-06-25 20:43:08 +0200

    final code refactoring

--- WordPressAutomation/Navigation/LeftNavigation.cs
+++ WordPressAutomation/Navigation/LeftNavigation.cs
@@ -2,20 +2,8 @@
 
 namespace WordPressAutomation
 {
-    public class LeftNavigation
+    public partial class LeftNavigation
     {
-        public class MenuSelector
-        {
-            /**
-             * Selects a particular menu given the menu id and link text
-             */
-            public static void Select(string topLevelMenuId, string subMenuLinkText)
-            {
-                Driver.Instance.FindElement(By.Id(topLevelMenuId)).Click();
-                Driver.Instance.FindElement(By.LinkText(subMenuLinkText)).Click();
-            }
-        }
-
         /**
          * Selects left navigation menu link
          */
---
+++ WordPressAutomation/Navigation/MenuSelector.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace WordPressAutomation
+{
+    public partial class LeftNavigation
+    {
+        public class MenuSelector
+        {
+            /**
+             * Selects a particular menu given the menu id and link text
+             */
+            public static void Select(string topLevelMenuId, string subMenuLinkText)
+            {
+                Driver.Instance.FindElement(By.Id(topLevelMenuId)).Click();
+                Driver.Instance.FindElement(By.LinkText(subMenuLinkText)).Click();
+            }
+        }
+    }
+}
